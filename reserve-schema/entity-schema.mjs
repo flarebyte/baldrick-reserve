44905e2098a2708b714513068de4294a4005adfd
Olivier Huin <olih@users.noreply.github.com>
2023-03-10 19:54:39 +0000

    Generate entity schema

--- reserve-schema/entity-schema.mjs
+++ reserve-schema/entity-schema.mjs
@@ -6,6 +6,22 @@ import { zodToJsonSchema } from "zod-to-json-schema";
  * Run: `npx zx --install reserve-schema/entity-schema.mjs`
  */
 
+const frequency = z.enum([
+  "never",
+  "once a year",
+  "once a month",
+  "once a week",
+  "once a day",
+  "once an hour",
+  "once a minute",
+  "once a second",
+]);
+
+const relationhship = z.object({
+  name: z.string(),
+  title: z.string(),
+});
+
 const entity = z.object({
   name: z.string(),
   title: z.string(),
@@ -15,6 +31,64 @@ const entity = z.object({
   shouldHave: z.array(z.string()),
   niceToHave: z.array(z.string()),
   shouldNotHave: z.array(z.string()),
+  retentionPeriod: z.enum(["year", "month", "week", "day"]),
+  createFrequency: frequency,
+  readFrequency: frequency,
+  updateFrequency: frequency,
+  deleteFrequency: frequency,
+  backupFrequency: frequency,
+  dataSize: z.enum(["B", "KB", "MB", "GB", "TB"]),
+  format: z.enum(["tabular", "key-value", "document", "graph", "time-series"]),
+  search: z.enum(["id", "text"]),
+  validation: z.array(
+    z.enum([
+      "basic",
+      "schema",
+      "service",
+      "access-service",
+      "curated",
+      "machine-learning",
+    ])
+  ),
+  ownership: z.array(z.enum(["public", "business", "individual"])),
+  parents: z.array(relationhship),
+  uses: z.array(relationhship),
+  fields: z.array(
+    z.enum([
+      "primitive",
+      "line",
+      "text",
+      "code",
+      "mime",
+      "image",
+      "media",
+      "url",
+      "ref",
+      "tuple",
+      "encrypted",
+      "hashed",
+      "secret",
+    ])
+  ),
+  privacy: z.array(
+    z.enum([
+      "personal",
+      "sensitive-special-category",
+      "privacy-high-risk",
+      "anonymous",
+      "pseudonymous",
+      "financial",
+      "children",
+      "criminal",
+      "ethnic",
+      "political",
+      "religious",
+      "trade-union",
+      "health",
+      "sexual-life",
+      "biometric",
+    ])
+  ),
 });
 
 const schema = z
