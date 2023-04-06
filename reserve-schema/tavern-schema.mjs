f572a6d49474eb639f458c64f7e94cbee02f45ee
Olivier Huin <olih@users.noreply.github.com>
2023-04-06 09:30:05 +0100

    Rethink of tavern schema

--- reserve-schema/tavern-schema.mjs
+++ reserve-schema/tavern-schema.mjs
@@ -4,73 +4,37 @@ import { zodToJsonSchema } from 'zod-to-json-schema';
 
 const smallParagraph = z.string().min(1).max(1000);
 
-const limits = z.object({
-  budget: z.object({
-    maxAmount: z.number().positive().max(10000).default(1),
-    targetAmount: z.number().positive().max(10000).default(0.5),
-    currency: z.enum(['pound', 'euro', 'dollar']).default('pound'),
-  }),
-  requests: z.number().positive().max(10000),
+const prompt = z.object({
+  text: smallParagraph,
 });
 
-const mainTag = z.enum(['creativity:creative']);
-const sectionTag = z.enum([
-  'creativity:creative',
-  'creativity:balanced',
-  'creativity:precise',
-  'output:image',
-]);
+const agentId = z.string().min(1).max(40);
 
-const engine = z.enum([
-  'openai:gpt-4',
-  'openai:instructGPT:ada',
-  'openai:instructGPT:babbage',
-  'openai:instructGPT:curie',
-  'openai:instructGPT:davinci',
-  'openai:dall-e',
-  'classify', // https://naturalnode.github.io/natural/bayesian_classifier.html
-  'digraph',
-  'fs:find',
-  'git:read',
-  'gh:read',
-  'json:read',
-  'yaml:read',
-  'csv:read',
-  'text:read',
-]);
-
-const section = z.object({
+const shellAgent = z.object({
+  kind: z.literal('shell'),
   title: z
     .string()
     .min(1)
     .max(80)
     .describe('Title summarizing the nature of the section'),
-  engine,
-  tags: z.array(sectionTag).max(50),
-  persona: z.string().min(1).max(60),
-  features: z
-    .array(smallParagraph)
-    .min(1)
-    .max(24)
-    .optional()
-    .describe('List of features detailing the section'),
+  shell: z.string().min(1).max(200),
 });
+
+const gptAgent = z.object({
+  kind: z.literal('openai:text'),
+  title: z
+    .string()
+    .min(1)
+    .max(80)
+    .describe('Title summarizing the nature of the section'),
+  prompts: z.array(prompt).min(1).max(9).describe('List of prompts'),
+});
+
+const agent = z.discriminatedUnion('kind', [shellAgent, gptAgent]);
 const schema = z
   .object({
-    title: z
-      .string()
-      .min(1)
-      .max(60)
-      .describe('Title summarizing the nature of the discussion'),
-    tags: z.array(mainTag).max(50),
-    sections: z
-      .array(section)
-      .min(1)
-      .max(16)
-      .describe(
-        'List of sections describing context, must have, should have, ...'
-      ),
-    limits,
+    title: z.string().min(1).max(60).describe('Title for the tavern group'),
+    agents: z.record(agentId, agent).describe('List of agents.'),
   })
   .describe('Model for specifying a discussion for Baldrick tavern');
 
