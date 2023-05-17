da341642a55bb9e3702e839854d684d8cac0c41e
Olivier Huin <olih@users.noreply.github.com>
2023-05-17 09:07:34 +0100

    Adds schema for service cost

---
+++ reserve-schema/service-cost-schema.mjs
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import { z } from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
+import { describeEnum } from './tavern-common.mjs';
+
+const shortName = z.string().min(1).max(40);
+const shortTitle = z.string().min(1).max(60);
+const conciseParagraph = z.string().min(1).max(600);
+const unit = z.string().min(1).max(40).describe('Unit of the data');
+const tagValues = {
+  human: 'Service provide by human',
+  webservice: 'Web service',
+};
+
+const currencyValues = {
+  dollar: 'American dollar',
+  gbp: 'British pound',
+  euro: 'Euro',
+};
+const tag = z
+  .enum(Object.keys(tagValues))
+  .describe(describeEnum('Tag', tagValues));
+
+const currency = z
+  .enum(Object.keys(currencyValues))
+  .describe(describeEnum('Currency', currencyValues));
+
+const serviceCost = z.object({
+  title: shortTitle.describe('Short title for the service cost'),
+  description: conciseParagraph
+    .describe('Description for the service cost')
+    .optional(),
+  amount: z.number().positive(),
+  tags: z.array(tag).min(1).max(10),
+  unit,
+});
+
+const schema = z
+  .object({
+    title: shortTitle.describe('Title for the list of service costs'),
+    currency,
+    year: z.number().positive().min(1900).max(3000),
+    costs: z.record(shortName, serviceCost),
+  })
+  .describe('Service cost');
+
+const jsonSchema = zodToJsonSchema(schema, 'service-cost-schema');
+
+await fs.writeJson('reserve-schema/service-cost.schema.json', jsonSchema, {
+  spaces: 2,
+});
